1f4ae2aeed527940024fca7a2c9bcf7f2738c7eb
Udunen <cole.huffine@gmail.com>
2024-12-04 20:50:58 -0600

    tank drive (kinda, rotation is wacky)

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -15,16 +15,15 @@ public class Movement : MonoBehaviour
 
     void FixedUpdate()
     {
-        float zDir = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
-        float xDir = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
-        rb.velocity = new Vector3(xDir, rb.velocity.y, zDir);
+        float move = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+        // float xDir = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
+        transform.Translate(transform.forward * move * Time.deltaTime, Space.World);
 
-        
 
         // rotation stuff (play with this later when u code tank drive)
-        // float rotation = Input.GetAxis("Horizontal") * turnSpeed * Time.deltaTime;
-        // Vector3 m_EulerAngleVelocity = new Vector3(0, rotation, 0);
-        // Quaternion deltaRotation = Quaternion.Euler(m_EulerAngleVelocity);
-        // rb.MoveRotation(rb.rotation * deltaRotation);
+        float rotation = Input.GetAxis("Horizontal") * turnSpeed * Time.deltaTime;
+        Vector3 m_EulerAngleVelocity = new Vector3(0, rotation, 0);
+        Quaternion deltaRotation = Quaternion.Euler(m_EulerAngleVelocity);
+        rb.MoveRotation(rb.rotation * deltaRotation);
     }
 }
